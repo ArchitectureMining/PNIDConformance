8927042e2f65744f821153b789701355f4aa80d3
Janjaap Ree <janjaapree@gmail.com>
2021-04-13 11:08:08 +0200

    Comment improvement

--- PNIDConformance/src/org/architecturemining/pnidconformance/algorithms/naivecc/NaiveCC.java
+++ PNIDConformance/src/org/architecturemining/pnidconformance/algorithms/naivecc/NaiveCC.java
@@ -9,7 +9,8 @@ import org.architecturemining.pnid.models.xlog.LogModel;
  * @author J. Ree
  * 
  * A naive approach to conformance checking on a Petri net with identifiers (PNID).
- * Given a PNID and a trace with identifier, computes the corresponding transition system, and find the closest firing sequence in the transition system, a.k.a. alignment.   
+ * Given a PNID and a trace with identifier, flattens the PNID to a classic petri net (up to a bound of identifiers),
+ * computes the corresponding transition system, and finds the closest firing sequence in the transition system, a.k.a. alignment.   
  *
  */
 public class NaiveCC {
